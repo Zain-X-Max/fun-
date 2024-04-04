e61a6aaf99b4601ef31cee36b97ea2963831db58
Zain-X-Max <164930492+Zain-X-Max@users.noreply.github.com>
2024-04-04 17:57:36 +0500

    test

---
+++ script.js
@@ -0,0 +1,88 @@
+
+const newLocal = <script>
+  function redirectToSecondPage() {window.location.assign("second_page.html")}
+
+
+
+  function redirectToThirdPage(){window.location.assign("third_page.html")}
+
+
+
+  function redirectTofourPage() {window.location.assign("four_page.html")}
+
+
+
+  function redirectTofivePage() {window.location.assign("five_page.html")}
+
+
+
+  function redirectTosixPage() {window.location.assign("six_page.html")}
+
+
+
+  function redirectTosevenPage() {window.location.assign("seven_page.html")}
+
+
+
+  function redirectToeightPage() {window.location.assign("eight_page.html")}
+
+
+
+  function redirectToninePage() {window.location.assign("nine_page.html")}
+
+
+
+  function redirectTotenPage() {window.location.assign("ten_page.html")}
+
+
+
+  function redirectToelevenPage() {window.location.assign("eleven_page.html")}
+
+
+  function redirectTotwelvePage() {window.location.assign("twelve_page.html")}
+
+
+
+  function redirectTothirteenPage() {window.location.assign("thirteen_page.html")}
+
+
+
+  function redirectTofourteenPage() {window.location.assign("fourteen_page.html")}
+
+
+
+  function redirectTofifteenPage() {window.location.assign("fifteen_page.html")}
+
+
+  function redirectTosixteenPage() {window.location.assign("sixteen_page.html")}
+
+
+
+  function redirectToseventeenPage() {window.location.assign("seventeen_page.html")}
+
+
+
+  function redirectToeighteenPage() {window.location.assign("eighteen_page.html")}
+
+
+  function redirectTonineteenPage() {window.location.assign("nineteen_page.html")}
+
+  function redirectTotwentyPage() {window.location.assign("twenty_page.html")}
+
+
+  function redirectTotwentyonePage() {window.location.assign("twentyone_page.html")}
+
+
+
+  function redirectTotwentytwoPage() {window.location.assign("twentytwo_page.html")}
+
+
+
+  function redirectTotwentythreePage() {window.location.assign("twentythree_page.html")}
+
+
+
+  function redirectTotwentyfourPage() {window.location.assign("twentyfour_page.html")}
+  <script src="script.js"></script>
+
+</script>;
